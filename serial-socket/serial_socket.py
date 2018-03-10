15d5be26c17212a9dcd485eb30134111070f7434
Kevin Qiu <kevinqiu06@gmail.com>
2018-03-10 01:53:17 +0000

    adding serial buffer

--- serial-socket/serial_socket.py
+++ serial-socket/serial_socket.py
@@ -4,11 +4,15 @@ SERIAL_PORT = '/dev/ttyAMA0'
 SERIAL_BAUDRATE = 9600
 
 ser = serial.Serial("/dev/ttyS0", baudrate=9600)
-msg = []
+msg = ''
+
+print("Serial connected!")
 
 while True:
     for c in ser.read():
-        msg.append(c)
-        if c == '\r':
+        char = chr(c)
+        msg += char
+        print(char)
+        if char == '\r':
             print('Message: ' + msg)
-            msg = []
+            msg = ''

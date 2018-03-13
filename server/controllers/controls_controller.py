bbbd33b7a838c0bcee9da7211e2c6c45ca349658
Kevin Qiu <kevinqiu96@gmail.com>
2018-03-13 14:16:10 -0700

    fixing datetime stuff

--- server/controllers/controls_controller.py
+++ server/controllers/controls_controller.py
@@ -4,7 +4,6 @@ from flask import Blueprint, abort, request
 
 from server.extensions import db, ser
 from server.helpers.gps_helper import find_closest_warehouse
-from server.models.item_data import ItemData
 from server.models.item_model import Item
 from server.models.warehouse_model import Warehouse
 
@@ -13,13 +12,10 @@ controls = Blueprint('controls', __name__)
 
 @controls.route('/controls/capture', methods=['POST'])
 def capture_image():
-    datetime = request.args.get('datetime')
-    latitude = request.args.get('latitude')
-    longitude = request.args.get('longitude')
-
-    print(datetime)
-    print(latitude)
-    print(longitude)
+    dt = datetime.strptime(request.json.get('datetime').split('+')[0],
+                           "%Y-%M-%DT%H:%M:%S.%f")
+    lat = request.json.get('latitude')
+    long = request.json.get('longitude')
 
     new_id = db.session.query(db.func.max(Item.id)).scalar() + 1
 
@@ -31,12 +27,12 @@ def capture_image():
     #category_id = ML GOES HERE
 
     # get closest warehouse location
-    closest_warehouse = find_closest_warehouse(Warehouse.query.all(), latitude, longitude)
+    closest_warehouse = find_closest_warehouse(Warehouse.query.all(), lat, long)
 
     # save item in DB
     item = Item(warehouse_id=closest_warehouse.id,
                 category_id=category_id,
-                datetime=datetime,
+                datetime=dt,
                 image_path='\images\{0}.jpeg'.format(new_id))
     db.session.add(item)
     db.session.commit()

b27ed033192857128f9c012d218438beea47335b
valrat <leri.ratu@gmail.com>
2018-04-02 11:44:56 -0700

    made videostream faster

--- camera/camera.py
+++ camera/camera.py
@@ -12,24 +12,24 @@ class Camera:
 
     def __init__(self):
         self.img_dir = None
+        self._cam = None
 
     def init_camera(self, img_dir):
         self.img_dir = img_dir
-
+        if platform.uname()[4].startswith('arm'):
+            # Running on the pi
+            self._cam = picamera.PiCamera()
+        else:
+            self._cam = cv2.VideoCapture(1)
+        
     def capture(self, img_id):
         name = str(img_id) + '.jpeg'
         name = os.path.join(self.img_dir, name)
         if platform.uname()[4].startswith('arm'):
-            # Running on the pi
-            camera = picamera.PiCamera()
-            camera.capture(name)
-            camera.close()
+            self._cam.capture(name)
         else:
-            camera = cv2.VideoCapture(1)
-            time.sleep(0.1)
-            _, image = camera.read()
+            _, image = self._cam.read()
             cv2.imwrite(name, image)
-            camera.release()
         return name
 
     def get_image(self):
@@ -38,9 +38,8 @@ class Camera:
             raise BaseException('Is not implemented')
             return ""
         else:
-            camera = cv2.VideoCapture(0)
-            _, image = camera.read()
-            frame = cv2.resize(image, (100, 27))
-            _, jpeg = cv2.imencode('.jpg', frame)
+            _, image = self._cam.read()
+            #frame = cv2.resize(image, (100, 27))
+            _, jpeg = cv2.imencode('.jpg', image)
             return jpeg
 
